c25fc5170a3a9f4c9113b5f2d4e6c9d6ba33f6ff
marwansheriff <marwan.sherrrf2@gmail.com>
2024-12-22 05:15:21 +0200

    v2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let isDrawing = false;  // Track whether the user is currently drawing
 function drawBasicShape(type, color) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);  // Clear canvas
   ctx.fillStyle = color;
-  
+
   if (type === 'circle') {
     ctx.beginPath();
     ctx.arc(canvas.width / 2, 250, 100, 0, 2 * Math.PI);  // Draw a circle
@@ -30,9 +30,9 @@ function drawCustomShape(points, color) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);  // Clear canvas
   ctx.fillStyle = color;
   ctx.beginPath();
-  
+
   ctx.moveTo(points[0].x, points[0].y);
-  
+
   for (let i = 1; i < points.length; i++) {
     ctx.lineTo(points[i].x, points[i].y);
   }
@@ -40,65 +40,84 @@ function drawCustomShape(points, color) {
   ctx.fill();
 }
 
-// // Example custom shape points (blob-like shape)
-// const blobPoints = [
-//   { x: 150, y: 200 },
-//   { x: 250, y: 150 },
-//   { x: 350, y: 200 },
-//   { x: 300, y: 300 },
-//   { x: 200, y: 300 }
-// ];
-
 // Free drawing functionality
 function enableFreeDraw(mode) {
-    if (mode===1){
-        canvas.onmousedown = () => {
-            isDrawing = true;
-            ctx.beginPath();
-        };
-        
-        canvas.onmousemove = (e) => {
-            if (isDrawing) {
-                ctx.lineTo(e.clientX, e.clientY-130);
-                ctx.stroke();
-    }
-};
+  if (mode === 1) {
+    canvas.onmousedown = () => {
+      isDrawing = true;
+      ctx.beginPath();
+    };
 
-canvas.onmouseup = () => {
-    isDrawing = false;
-    ctx.closePath();
-};
-}
-else if(mode===2){
-  x=0
-    ctx.beginPath();
-    canvas.onclick =(e) =>{
-      if(x===0){
-        ctx.moveTo(e.clientX, e.clientY-130);
-        x=1
+    canvas.onmousemove = (e) => {
+      if (isDrawing) {
+        ctx.lineTo(e.clientX, e.clientY - 130);
+        ctx.stroke();
       }
-      else{
-    ctx.lineTo(e.clientX,e.clientY-130);
-    ctx.stroke();
+    };
+
+    canvas.onmouseup = () => {
+      isDrawing = false;
+      ctx.closePath();
+    };
+  } else if (mode === 2) {
+    let hasStarted = false;
+    canvas.onclick = (e) => {
+      if (!hasStarted) {
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY - 130);
+        hasStarted = true;
+      } else {
+        ctx.lineTo(e.clientX, e.clientY - 130);
+        ctx.stroke();
+      }
+    };
   }
 }
-}
+
+
+function randomshape() {
+  const midX = canvas.width / 2;
+  const midY = canvas.height / 2;
+  const outlineOnly = true;
+  const variationFactor = Math.random() * 50 + 50; // Randomize variation factor between 50 and 100
+
+  ctx.beginPath();
+  const totalPoints = 10; 
+  for (let point = 0; point < totalPoints; point++) {
+    const theta = (2 * Math.PI * point) / totalPoints;
+    const offset = 100 + Math.random() * variationFactor; // Randomize radius
+    const coordX = midX + offset * Math.cos(theta);
+    const coordY = midY + offset * Math.sin(theta);
+    if (point === 0) {
+      ctx.moveTo(coordX, coordY);
+    } else {
+      ctx.lineTo(coordX, coordY);
+    }
+  }
+  ctx.closePath();
+
+  if (outlineOnly) {
+    ctx.stroke();
+  } else {
+    ctx.fill();
+  }
 }
 
 // Button click handler
 document.getElementById('drawBtn').addEventListener('click', function () {
-  ctx.closePath();
-  ctx.clearRect(0,0,canvas.width,canvas.height)
-const selectedShape = document.getElementById('shape').value;
-const selectedColor = document.getElementById('color').value;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const selectedShape = document.getElementById('shape').value;
+  const selectedColor = document.getElementById('color').value;
 
-ctx.strokeStyle = selectedColor;
-ctx.fillStyle = selectedColor;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
 
- if (selectedShape === 'FREE1') {
+  if (selectedShape === 'FREE1') {
     enableFreeDraw(1);  // Activate free drawing mode
   } else if (selectedShape === 'FREE2') {
     enableFreeDraw(2);  // Activate free drawing mode
+  } else if (selectedShape === 'randomshape') {
+    randomshape();
   } else {
     drawBasicShape(selectedShape, selectedColor);
   }
